9f7820b09543d694efc798f5636396599014f6c5
friquelmel9 <friquelmel.137@gmail.com>
2025-02-17 17:04:41 -0300

    Creacion BoxQuestion     - Permite visualizar una pregunta     - Por el momento la pregunta es una random, no seleccionable     - Misma visualizacion que ya se tenia de antes

---
+++ src/componentes/QuestionBox.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import '/src/componentes/QuestionBox.css';
+
+// question is the question to show, boxState the type of box
+const QuestionBox = ({ question, boxState }) => {
+
+    if (boxState == "visualizacion") {
+        return (
+            <div className="container">
+                {question.id == null ? (
+                    <p className="enunciado">Seleccione una pregunta</p>
+                ) : (
+                    <>
+                        <div className="top-bar">
+                            <div class="id-number"># {question.id}</div>
+                            <div class="top-right-elements">{question.referencia}</div>
+                        </div>
+                        <div className="enunciado">{question.pregunta} </div>
+                        {question.respuesta == "V" | question.respuesta == "F" ? (
+                            <div className="alternativas-container">
+                                <button class="button">V</button>
+                                <button class="button">F</button>
+                            </div>
+                        ) : (
+                            <>
+                                <button class="button">Option 1</button>
+                                <button class="button">Option 1</button>
+                                <button class="button">Option 1</button>
+                                <button class="button">Option 1</button>
+                                <button class="button">Option 1</button>
+                            </>
+                        )}
+                    </>
+                )}
+            </div>
+        )
+    }
+
+}
+
+export default QuestionBox
--- src/vistas/Documentacion.jsx
+++ src/vistas/Documentacion.jsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import QuestionBox from '/src/componentes/questionBox.jsx'
 
 function Documentacion() {
 
+    const nullQuestion = {
+        id: null
+    }
+
+    const exampleQuestion = {
+        referencia: "[2024-2] - Jose Luis Marti. Q1P1",
+        pregunta: "\nCual de las siguientes definiciones respecto a la sintaxis son correctas?\n    I. Sintaxis lexica: Define reglas para los tokens (tales como identificadores, literales, operadores...)\n    II. Sintaxis concreta: Refiere a una representacion de un programa segun simbolos del alfabeto\n    III. Sintaxis abstracta: Solo lleva la informacion escencial del programa, util para usar previo a la generacion del codigo\na. Solo II\nb. Solo I y II\nc. Solo II y III\nd. Todas las anteriores\n\n",
+        respuesta: "V",
+        explicacion: "No explicacion entregada",
+        id: 67
+    }
+
+    const [selectedQuestion, setSelectedQuestion] = useState(exampleQuestion)
+    const [questionsArray, setQuestionsArray] = useState()
+    const [boxState, setBoxState] = useState()
+    
 
     return (
         <>
             <h1>Titulo Momentaneo</h1>
+            <QuestionBox question={selectedQuestion} boxState="visualizacion"></QuestionBox>
         </>
     );
 }
