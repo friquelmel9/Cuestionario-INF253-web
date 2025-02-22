adc19548a15ca62df7ceeb906e406faed61eff55
friquelmel9 <friquelmel.137@gmail.com>
2025-02-22 13:08:31 -0300

    Documentacion Filtro por Quiz

--- src/vistas/Documentacion.jsx
+++ src/vistas/Documentacion.jsx
@@ -15,20 +15,15 @@ function Documentacion() {
         id: null
     }
 
-    const exampleQuestion = {
-        referencia: "[2024-2] - Jose Luis Marti. Q1P1",
-        pregunta: "\nCual de las siguientes definiciones respecto a la sintaxis son correctas?\n    I. Sintaxis lexica: Define reglas para los tokens (tales como identificadores, literales, operadores...)\n    II. Sintaxis concreta: Refiere a una representacion de un programa segun simbolos del alfabeto\n    III. Sintaxis abstracta: Solo lleva la informacion escencial del programa, util para usar previo a la generacion del codigo\na. Solo II\nb. Solo I y II\nc. Solo II y III\nd. Todas las anteriores\n\n",
-        respuesta: "D",
-        explicacion: "No explicacion entregada",
-        id: 100
-    }
-
     const [selectedQuestion, setSelectedQuestion] = useState(nullQuestion)
     const [questionsArray, setQuestionsArray] = useState([])
+
+    const [selectedFilter, setSelectedFilter] = useState(-1)
     const [filteredArray, setFilteredArray] = useState([])
 
     const [showQuestion, setShowQuestion] = useState(false)
     
+    // Arch
     useEffect(() => {
         const processJsonData = (data, type, quiz) => {
             return data.map(item => ({ ...item, type, quiz }));
@@ -52,6 +47,16 @@ function Documentacion() {
         setQuestionsArray([...vfQuestions, ...altQuestions]);
     }, []);
 
+    const handleFilterChange = (event) => {
+        const selectedFilter = parseInt(event.target.value);
+        setSelectedFilter(selectedFilter)
+    }
+
+    const handleFilterArray = (event) => {
+        const selectedFilter = parseInt(event.target.value);
+        setFilteredArray(questionsArray.filter(item => item.quiz === selectedFilter) || nullQuestion )
+    }
+
     const handleSelectedChange = (event) => {
         const selectedId = parseInt(event.target.value);
         setSelectedQuestion(questionsArray.find(item => item.id === selectedId) || nullQuestion )
@@ -62,18 +67,46 @@ function Documentacion() {
         <>
             <TopMenu text="Ir al Inicio" link={`${import.meta.env.BASE_URL}Inicio`} />
             <div>
-                <select onChange={handleSelectedChange}>
+                <select onChange={handleFilterChange}>
                     <option value={-1}>--Seleccione--</option>
-                    {questionsArray.map(item => (
-                        <option value={item.id}>
-                            {item.id}
-                        </option>
-                    ))}
+                    <option value={1}>No filtro</option>
+                    <option value={2}>Filtrar por Quiz</option>
+                    <option value={3}>Filtrar por tema (no disponible)</option>
                 </select>
+                
+                {selectedFilter === 2 && (
+                    <select onChange={handleFilterArray}>
+                        <option value={-1}>--Seleccione--</option>
+                        <option value={1}>Quiz 1</option>
+                        <option value={2}>Quiz 2</option>
+                        <option value={3}>Quiz 3</option>
+                        <option value={4}>Quiz 4</option>
+                        <option value={5}>Quiz 5</option>
+                    </select>
+                )}
+
+                {!(selectedFilter === -1) && (
+                    <select onChange={handleSelectedChange}>
+                    <option value={-1}>--Seleccione--</option>
+                    {selectedFilter === 2 && (
+                        filteredArray.map(item => (
+                            <option value={item.id}>{item.id}</option>
+                        ))
+                    )}
+                    {selectedFilter === 1 && (
+                        questionsArray.map(item => (
+                            <option value={item.id}>{item.id}</option>
+                        ))
+                    )} 
+                    
+                    </select>
+                )}
             </div>
             <QuestionBox question={selectedQuestion} mostrarRespuesta={showQuestion} isTest={false}></QuestionBox>
         </>
     );
 }
+
+
 
 export default Documentacion
