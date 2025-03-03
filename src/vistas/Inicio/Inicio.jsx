297895048c591c7095f1e2934a5dcbfdf7fd78f0
AyuJohn <johnalvaradotoro@hotmail.com>
2025-03-02 23:40:37 -0300

    Pequeno cambio a los botones del inicio, y creacion del FooterMenu

--- src/vistas/Inicio/Inicio.jsx
+++ src/vistas/Inicio/Inicio.jsx
@@ -3,65 +3,65 @@ import TopMenu from "@/vistas/TopMenu/TopMenu";
 
 function Inicio() {
   const [count, setCount] = useState(0);
-
+  
   return (
     <>
-      <TopMenu text='' link='' inicio={true}/>  
-
-      {/* Material de estudio Personal */}
-      <div className='container mt-4'>
-      <h3>Material de Estudio Personal</h3>
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '25px'}}>
-
-          {/* Practica */}
-          <div className='border p-4 rounded' style={{width: '80%', display: 'flex', flexDirection: 'column'}}>
-            <h4> Practicar </h4>
-            <p style={{textAlign: 'justify'}}>
-              Aquí podras realizar <strong>quices</strong> y <strong>certamenes</strong> de 
-              prueba para practicar tus habilidades y conocimientos adquiridos en las clases 
-              de <strong>Lenguajes de Programación</strong>, ademas de poder seleccionar los 
-              contenidos a estudiar y el tipo de preguntas que desees practicar.
-            </p>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-              <button
-                className="btn btn-primary w-80"
-                type="button" 
-                onClick={() => window.location.href = `${import.meta.env.BASE_URL}ConfigurarEvaluacion`}>
-                Simulacion de Cuestionario
-              </button>
-            </div>
-
-          </div>
-          {/* FIN Practica */}
+      <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+        <TopMenu text='' link='' inicio={true}/>  
+        <div style={{display: 'flex',justifyContent: 'center'}}>
+          <h1><strong>Cuestionario INF-253</strong></h1>
+        </div>
+        {/* Material de estudio Personal */}
+        <div className='container mt-4'>
+        <h3>Material de Estudio Personal</h3>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '25px'}}>
 
 
 
-          {/* Documentacion */}
-          <div className='border p-4 rounded' style={{width: '80%', display: 'flex', flexDirection: 'column'}}>
-            <h4> Revisar Documentación </h4>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
+            {/* Practica */}
+            <div className='border p-4 rounded' style={{width: '80%', display: 'flex', flexDirection: 'column'}}>
+              <h4> Practicar </h4>
               <p style={{textAlign: 'justify'}}>
-                Aquí podras revisar las preguntas que se encuentran en el material del curso, 
-                ademas podras encontrar sus respectivas respuestas y su explicación para mejorar 
-                tu estudio y preparación.
+                Aquí podras realizar <strong>quices</strong> y <strong>certamenes</strong> de 
+                prueba para practicar tus habilidades y conocimientos adquiridos en las clases 
+                de <strong>Lenguajes de Programación</strong>, ademas de poder seleccionar los 
+                contenidos a estudiar y el tipo de preguntas que desees practicar.
               </p>
               <div style={{display: 'flex', justifyContent: 'center'}}>
-                <button
-                  className="btn btn-primary w-80"
-                  type="button"
-                  onClick={() => window.location.href = `${import.meta.env.BASE_URL}Documentacion`}>
-                  Documentación
-                </button>
+                <a 
+                  href={`${import.meta.env.BASE_URL}ConfigurarEvaluacion`} 
+                  className="btn btn-primary w-80">
+                     Simulacion de Cuestionario
+                </a>
               </div>
             </div>
+
+
+
+            {/* Documentacion */}
+            <div className='border p-4 rounded' style={{width: '80%', display: 'flex', flexDirection: 'column'}}>
+              <h4> Revisar Documentación </h4>
+              <div style={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
+                <p style={{textAlign: 'justify'}}>
+                  Aquí podras revisar las preguntas que se encuentran en el material del curso, 
+                  ademas podras encontrar sus respectivas respuestas y su explicación para mejorar 
+                  tu estudio y preparación.
+                </p>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                  <a
+                    href={`${import.meta.env.BASE_URL}Documentacion`} 
+                    className="btn btn-primary w-80">
+                    Documentación  
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+          
           </div>
-          {/* FIN Documentacion */}
-
-
-
         </div>
       </div>
-      {/* FIN Material de estudio Personal */}
     </>
   );
 };
