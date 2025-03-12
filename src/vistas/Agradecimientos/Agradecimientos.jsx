20623f927d6b46178e80c09b6cbfc13c7a88f695
John <johnalvaradotoro@hotmail.com>
2025-03-12 13:56:14 -0300

    Esuqeleto Pagina de Agradecimientos

--- src/vistas/Agradecimientos/Agradecimientos.jsx
+++ src/vistas/Agradecimientos/Agradecimientos.jsx
@@ -8,6 +8,19 @@ function Agradecimientos(){
         <>
             <div style={{display: 'flex', flexDirection: 'column', gap: '20px', minHeight:'100vh'}}>
                 <TopMenu text="Ir al Inicio" link={`${import.meta.env.BASE_URL}Inicio`} />
+                <h1 style={{textAlign: 'center'}} > Agradecimientos</h1>
+                <div 
+                    className={`container mt-4 ${isDarkTheme ? 'bg-dark border border-white' : 'bg-light shadow'}`} 
+                    style={{display: 'flex', flexFlow: 'column', justifyContent:'space-evenly'}}>
+                    {/* Espacio para la persona */}
+                    <div 
+                        className={`border p-4 rounded ${isDarkTheme ? 'border-white' : 'border-black'}`}
+                        style={{display: 'flex', flexFlow: 'column', justifyContent:'space-evenly', minHeight:'200px'}}>
+                        
+                    </div>
+                    {/* Espacio para la persona es copiar y pegar para mas agradecimientos */}
+
+                </div>
             </div>
             <FooterMenu inicio={false}/> 
         </>
